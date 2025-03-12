1dc0fd44cec10638dec20e01ebbd5ec722f5d114
TatySCV <tatiana.campos03@inacapmail.cl>
2025-03-12 09:29:40 -0300

    initialize React app with Vite, add routing and basic pages

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,10 @@
+const AboutPage = () => {
+    return (
+      <div>
+        <h1>Acerca de</h1>
+        <p>Información sobre la aplicación.</p>
+      </div>
+    );
+  };
+  
+  export default AboutPage;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+    return (
+      <div>
+        <h1>Bienvenido al Sistema de Gestión de Condominios</h1>
+        <p>Esta es la página principal.</p>
+      </div>
+    );
+  };
+  
+  export default HomePage;
